712a49fee1e9402e535724d431d82a7d6de2c345
Guilherme P.Oliveira <guilhermeponciano3225@gmail.com>
2024-11-18 15:15:12 -0300

    adicionando mais interatividade do cliente e deixando resposivo o header/cabeça

---
+++ script.js
@@ -0,0 +1,42 @@
+//menu para mobile
+var iconeMenu=document.getElementById('menu-icone');
+var header=document.getElementById('cabeca');
+//logo-interaçao
+var logo=document.getElementById('logo-preto')
+//abrir menu
+function abrirMenu(){
+        iconeMenu.addEventListener('click',()=>{
+        iconeMenu.style.transform='rotate(90deg)'
+        iconeMenu.style.transition='250ms'
+        header.classList.add("fechar-menu")
+        header.classList.remove("fechar-menu")
+        header.classList.add("adicionar-menu")
+            fecharMenu()
+    })
+
+}abrirMenu()
+//fechamento do menu
+function fecharMenu(){
+    iconeMenu.addEventListener('click',()=>{
+        iconeMenu.style.transform='rotate(0deg)'
+        iconeMenu.style.transition='250ms'
+        header.classList.add("fechar-menu")
+        abrirMenu()
+})
+}
+
+function logoPreta(){
+    logo.addEventListener('click',()=>{
+        logo.setAttribute('src','imagens/logo-Nexis-01.png')
+        logo.style.filter='drop-shadow(0px 0px 10px #00fffb)'
+        logoBranca()
+    })
+}logoPreta()
+
+function logoBranca(){
+    logo.addEventListener('click',()=>{
+        logo.setAttribute('src','imagens/logo-preto-1.png')
+        logo.style.filter='drop-shadow(0px 0px 10px gray)'
+        logoPreta()
+    })
+}
